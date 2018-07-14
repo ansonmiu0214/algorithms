6dd9140b0aba3c36eade7ab6ab6c0f5d882a01c5
Anson Miu <ansonmiu0214@gmail.com>
2018-07-14 20:59:43 +0800

    Added ADT file with TreeNode and ListNode

---
+++ adt.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+class ListNode:
+	def __init__(self, val):
+		self.val = val
+		self.next = None
+
+	def __repr__(self):
+		return str(self.val) + ("->{}".format(self.next) if self.next else "")
+
+
+def array_to_list(vals):
+	res = list(map(lambda x: ListNode(x), vals))
+	for i in range(len(vals) - 1):
+		res[i].next = res[i + 1]
+
+	return res[0]
+
+class TreeNode:
+	def __init__(self, val):
+		self.val = val
+		self.left = None
+		self.right = None
+
+	def __repr__(self):
+		return "Node {} ({}) ({})".format(self.val, self.left or "", self.rght or "")
