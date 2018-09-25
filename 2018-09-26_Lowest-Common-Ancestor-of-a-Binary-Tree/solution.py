3d45042fe871ce7a5e59a85834a164c61088be48
Anson Miu <ansonmiu0214@gmail.com>
2018-09-25 17:25:46 +0800

    Initialised solution for lowest-common-ancestor-of-binary-tree problem

---
+++ 2018-09-26_Lowest-Common-Ancestor-of-a-Binary-Tree/solution.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+import sys
+sys.path.append('../')
+
+from adt import TreeNode
+
+def lowestCommonAncestor(root, p, q):
+  # collect parent information
+  # `parents' dictionary :: node_value -> parentNode
+  parents = dict()
+  pass
+
+
+
