d7a38df50d96a1b4803224bad77b2c3b64e3efa3
Anson Miu <ansonmiu0214@gmail.com>
2018-12-21 10:44:23 +0800

    Added array_to_tree builder method

--- adt.py
+++ adt.py
@@ -1,5 +1,7 @@
 #!/bin/python3
 
+from collections import deque
+
 class ListNode:
 	def __init__(self, val):
 		self.val = val
@@ -24,3 +26,33 @@ class TreeNode:
 
 	def __repr__(self):
 		return "Node {} ({}) ({})".format(self.val, self.left or "", self.right or "")
+
+
+def array_to_tree(vals):
+  if not vals:
+    return None
+
+  first, *rest = vals
+  root = TreeNode(first)
+
+  end = len(rest)
+
+  queue = deque([root])
+  i = 0
+  while i < end:
+    node = queue.popleft()
+    if rest[i]:
+      node.left = TreeNode(rest[i])
+      queue.append(node.left)
+
+    i += 1
+    if i == end:
+      break
+
+    if rest[i]:
+      node.right = TreeNode(rest[i])
+      queue.append(node.right)
+    i += 1
+
+  return root
+
